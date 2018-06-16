6ab444ca9fdcdab0a7a3e1e4e784453fe079f298
Simon <simon.phommarath@gmail.com>
2018-06-16 13:29:15 -0400

    Updated CSS Updated Interest Section Updated footer Fixed Parallax

--- script/script.js
+++ script/script.js
@@ -1,9 +1,3 @@
-$('.parallax-window').parallax({
-    naturalWidth: 5312,
-    naturalHeight: 2988
-  });
-
-
 $(document).ready(function () {
   $('body').css('display', 'none');
   $('body').fadeIn(1500);
